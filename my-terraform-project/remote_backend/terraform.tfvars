edbe010c80e7081d8503ba83b35794194ea19e13
zeeepii <ziedgormazi16@gmail.com>
2024-12-09 11:39:49 +0100

    config

--- my-terraform-project/remote_backend/terraform.tfvars
+++ my-terraform-project/remote_backend/terraform.tfvars
@@ -1,3 +1,3 @@
 # Optionally, you can override the default values
-s3_bucket_name     = "custom-terraform-state-bucket-123456-fca2a3da"
+s3_bucket_name     = "custom-terraform-state-bucket-123456"
 dynamodb_table_name = "custom-terraform-state-locks-123456"
--- my-terraform-project/remote_backend/variables.tf
+++ my-terraform-project/remote_backend/variables.tf
@@ -3,11 +3,11 @@
 variable "s3_bucket_name" {
   description = "Name of the S3 bucket to store Terraform state"
   type        = string
-  default     = "enis-terraform-for-state-file"
+  default     = "enis-terraform-for-state-file-0125"
 }
 # DynamoDB Table Name
 variable "dynamodb_table_name" {
   description = "Name of the DynamoDB table for state locking"
   type        = string
-  default     = "terraform-state-locking"
+  default     = "terraform-state-locking-1256"
 }
